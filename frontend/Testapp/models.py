2a1a6a9994a2ac23c8098d611713dfb507f0d40e
Dalton Adcock <daltonw.adcock@calbaptist.edu>
2022-07-13 15:07:32 -0700

    timestamps + unique ID logic, resets hourly :)

--- frontend/Testapp/models.py
+++ frontend/Testapp/models.py
@@ -1,22 +1,30 @@
+from django.conf.global_settings import MEDIA_ROOT
+from django.core.files.storage import default_storage
 from django.core.validators import FileExtensionValidator
 from django.db import models
-import uuid
+import os
+import datetime
 
 
-def path_wrapper(name, session):
+def path_wrapper(name):
 
     def user_directory_path(instance, filename):
-        return "session_" + session + "/" + name + "/" + filename
+        time_now = datetime.datetime.now()
+        check_dir = 0
+
+        while default_storage.exists("timestamp_" + time_now.now().strftime("%Y-%b-d_%d-hr_%H") + "id_" + str(check_dir) + "/" + name):
+            check_dir += 1
+
+        return "timestamp_" + datetime.datetime.now().strftime("%Y-%b-d_%d-hr_%H") + "id_" + str(check_dir) + "/" + name
+
     return user_directory_path
 
 
 class Information(models.Model):
-    my_filename = uuid.uuid4().hex
-
-    families = models.FileField(upload_to=(path_wrapper("families", my_filename)),
+    families = models.FileField(upload_to=(path_wrapper("families")),
                                 validators=[FileExtensionValidator(allowed_extensions=['xlsx', 'csv'])])
-    chromosomes = models.FileField(upload_to=(path_wrapper("chromosomes", my_filename)),
+    chromosomes = models.FileField(upload_to=(path_wrapper("chromosomes")),
                                    validators=[FileExtensionValidator(allowed_extensions=['txt'])])
-    colors = models.FileField(upload_to=(path_wrapper("colors", my_filename)), blank=True, null=True,
+    colors = models.FileField(upload_to=(path_wrapper("colors")), blank=True, null=True,
                               validators=[FileExtensionValidator(allowed_extensions=['txt'])])
 
